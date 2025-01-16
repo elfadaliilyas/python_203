b733cf360ab9292ec94c01475b9d82154f61acbb
elfadaliilyas <72802174+elfadaliilyas@users.noreply.github.com>
2025-01-16 19:32:59 +0100

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -7,5 +7,4 @@ class ForwardContract:
     purchase_date: pd.Timestamp
     maturity_date: pd.Timestamp
     forward_price: float
-    quantity: int  # Quantity of forward contracts
     position_type: str  # 'BUY' or 'SELL'
