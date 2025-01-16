9e29d721b4366ac7b214925e53fbbdaa2e6b8d88
elfadaliilyas <72802174+elfadaliilyas@users.noreply.github.com>
2025-01-16 19:38:26 +0100

    Update forward_broker.py
    This will include our broker agent who will account for the overall positions, purchase date and PnL

--- brokers/forward_broker.py
+++ brokers/forward_broker.py
@@ -1,1 +1,11 @@
+from pybacktestchain.broker import Backtest,Broker
+from dataclasses import dataclass
+import pandas as pd
+from datetime import datetime
 
+@dataclass
+class ForwardContract:
+    purchase_date: pd.Timestamp
+    maturity_date: pd.Timestamp
+    forward_price: float
+    position_type: str  # 'BUY' or 'SELL'
