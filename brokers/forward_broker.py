198823dbffc681447358f060a681dc5dfb62389a
elfadaliilyas <72802174+elfadaliilyas@users.noreply.github.com>
2025-01-16 21:48:21 +0100

    Update forward_broker.py
    Running the backtest function and cashing-in the payoff

--- brokers/forward_broker.py
+++ brokers/forward_broker.py
@@ -56,6 +56,25 @@ class ForwardTradingBroker(Broker):
             self.log_transaction(date, "SELL_FORWARD", 1, forward_price, 0)  # No upfront cost for selling
 
 
+    def close_expired_positions(self, current_date, actual_spot_price):
+        """
+        Closes all forward contracts that mature on or before the current date.
+
+        Args:
+            current_date (pd.Timestamp): The current date.
+            actual_spot_price (float): The actual spot price at the maturity date.
+        """
+        for pos in self.positions:
+            if pos.position_type == "BUY":
+                payoff = (actual_spot_price - pos.forward_price)
+            elif pos.position_type == "SELL":
+                payoff = (pos.forward_price - actual_spot_price)
+            self.cash += payoff   
+            self.list_payoff.append(self.cash)
+
+            self.behaviour.append(pos.position_type)
+            self.log_transaction(current_date, "CLOSE_;FORWARD", 1, pos.forward_price, payoff)
+            break
 
     def run_backtest(self, data: pd.DataFrame):
         """
@@ -66,3 +85,17 @@ class ForwardTradingBroker(Broker):
         """
         data['maturity_date'] = pd.to_datetime(data["Date"]) + pd.Timedelta(days=30)
         
+        for _, row in data.iterrows():
+            current_date = row['Date']
+            forward_price = row['Brent Futures']
+            predicted_spot = row['predicted_spot_price']
+            maturity_date = row['maturity_date']
+            actual_spot = row['actual_values']
+            # Trade based on prediction
+            self.trade_forward(current_date, forward_price, maturity_date, predicted_spot, actual_spot)
+            
+        # Close expired positions
+        self.close_expired_positions(current_date, data['actual_values'])
+        self.cash = pd.DataFrame(self.cash)["actual_values"].iloc[-1]
+        # Final portfolio value
+        return self.cash
